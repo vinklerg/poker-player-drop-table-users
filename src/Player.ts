e260433ec3cca928407f1c1d3ac7cc97cc2070fc
akoshegel <hegelakos@codingsans.com>
2019-12-20 11:14:41 +0100

    some

--- src/Player.ts
+++ src/Player.ts
@@ -4,7 +4,7 @@ export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
     const [player] = gameState.players.filter(player => player.name === 'DROP TABLE users');
     const holeCards = player['hole_cards'];
-    const bet = holeCards.any(card => {
+    const bet = holeCards.sum(card => {
       return !parseInt(card.rank);
     });
     if (bet) {
