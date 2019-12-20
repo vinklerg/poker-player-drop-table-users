d7a718c548ba5e29a43f640ce554f6944ac08eb6
akoshegel <hegelakos@codingsans.com>
2019-12-20 11:33:51 +0100

    ranks

--- src/Player.ts
+++ src/Player.ts
@@ -1,18 +1,43 @@
+enum Turns {
+  PREFLOP = 'preflop',
+  FLOP = 'flop',
+  TURN = 'turn',
+  RIVER = 'river',
+}
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
     const [player] = gameState.players.filter(player => player.name === 'DROP TABLE users');
+
+    const holeCards = player['hole_cards'];
+
+    const communityCards = gameState['community_cards'];
+
+    const turn = this.getTurn(communityCards);
+
     const playerBets = gameState.players.map(player => {
       return player.bet;
     });
 
     const maxBet = Math.max(playerBets);
+
     if (maxBet > player.bet) {
       return betCallback(maxBet - player.bet);
     }
 
-    const holeCards = player['hole_cards'];
-
-    const communityCards = gameState['community_cards'];
+    switch (turn) {
+      case Turns.PREFLOP: {
+        break;
+      }
+      case Turns.FLOP: {
+        break;
+      }
+      case Turns.TURN: {
+        break;
+      }
+      case Turns.RIVER: {
+        break;
+      }
+    }
 
     const rank = communityCards
       .map(card => card.rank)
@@ -25,6 +50,17 @@ export class Player {
     } else {
       betCallback(gameState['small_blind'] * 1);
     }
+  }
+
+  public getTurn(cCards) {
+    if (cCards.length === 3) {
+      return Turns.FLOP;
+    } else if (cCards.length === 4) {
+      return Turns.TURN;
+    } else if (cCards.length === 5) {
+      return Turns.RIVER;
+    }
+    return Turns.PREFLOP;
   }
 
   public showdown(gameState: any): void {}
