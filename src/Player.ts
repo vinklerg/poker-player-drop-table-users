fca58d51a48d816cd6b4c209cdc8bf6ee04f7dc4
akoshegel <hegelakos@codingsans.com>
2019-12-20 11:12:16 +0100

    feat(bet): ha jo lapunk van bet

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,17 @@
+import { S_IFREG } from 'constants';
+
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(50);
+    const [player] = gameState.players.filter(player => player.name === 'DROP TABLE users');
+    const holeCards = player['hole_cards'];
+    const bet = holeCards.any(card => {
+      return !parseInt(card.rank);
+    });
+    if (bet) {
+      betCallback(50);
+    } else {
+      betCallback(0);
+    }
   }
 
   public showdown(gameState: any): void {}
