7c074a694d9d6838bd82310cfbe63c3732ab8a6f
akoshegel <hegelakos@codingsans.com>
2019-12-20 11:20:28 +0100

    rank

--- src/Player.ts
+++ src/Player.ts
@@ -4,13 +4,19 @@ export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
     const [player] = gameState.players.filter(player => player.name === 'DROP TABLE users');
     const holeCards = player['hole_cards'];
-    const bet = holeCards.sum(card => {
-      return !parseInt(card.rank);
-    });
-    if (bet) {
+
+    const communityCards = gameState['community_cards'];
+
+    const rank = communityCards
+      .map(card => card.rank)
+      .some(communityCardRank => {
+        return holeCards.map(card => card.rank).some(cardRank => cardRank === communityCardRank);
+      });
+
+    if (rank) {
       betCallback(gameState['small_blind'] * 5);
     } else {
-      betCallback(0);
+      betCallback(gameState['small_blind'] * 1);
     }
   }
 
